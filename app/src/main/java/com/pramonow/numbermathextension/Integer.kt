bf781450879170ff305e37d1867fc16ee55f9ae2
Pramono <pramono_winata@yahoo.com>
2018-11-28 00:31:48 +0700

    Added files for several Variables, added functions and changed implementation

--- app/src/main/java/com/pramonow/numbermathextension/Integer.kt
+++ app/src/main/java/com/pramonow/numbermathextension/Integer.kt
@@ -1,29 +1,53 @@
 package com.pramonow.numbermathextension
 
 fun Int.square():Int {
-
-    this.times(this)
-    return this
+    return this*this
 }
 
 fun Int.cubic():Int {
-    this.times(this).times(this)
-    return this
+    return this*this*this
 }
 
 fun Int.power(pow:Int):Int{
 
+    var value = this
     repeat(pow-1) {
-        this.times(this)
+        value*this
     }
 
-    return this
+    return value
 }
 
 fun Int.absolute():Int {
     if(this < 0)
-        return this.times(-1)
-
+        return this*-1
     return this
 }
+
+fun Int.log():Int {
+
+    var value = 0;
+    var originalValue = this
+
+    while(this >= 2)
+    {
+        originalValue = originalValue/2
+        value++
+    }
+
+    return value
+}
+
+fun Int.factorial():Int{
+
+    if(this <= 1) return 1
+
+    var value = 1
+
+    for(i in 1..this)
+        value = value*i
+
+    return value
+}
+
 
