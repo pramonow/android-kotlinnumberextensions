422ba8fab7793fc5b56d316e9b6c2907625f8067
Pramono <pramono.winata@tokopedia.com>
2018-12-02 00:08:20 +0700

    Add unit test for double and float

--- app/src/test/java/com/pramonow/numbermathextension/DoubleUnitTest.kt
+++ app/src/test/java/com/pramonow/numbermathextension/DoubleUnitTest.kt
@@ -1,2 +1,103 @@
 package com.pramonow.numbermathextension
 
+import org.junit.Assert
+import org.junit.Test
+
+class DoubleUnitTest {
+
+    @Test
+    fun squareIsCorrect() {
+        var number = 4.0
+        var anotherNumber = 10.0
+
+        if(number.square() == 16.0 && anotherNumber.square() == 100.0)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun cubicIsCorrect() {
+        var number = 4.0
+        var anotherNumber = 10.0
+
+        if(number.cubic() == 64.0 && anotherNumber.cubic() == 1000.0)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun powerIsCorrect() {
+        var number = 4.0
+
+        if(number.power(0) == 1.0 && number.power(3) == 64.0)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun absoluteIsCorrect() {
+        var number = 4.0
+        var anotherNumber = -4.0
+
+        if(number.absolute() == 4.0 && anotherNumber.absolute() == 4.0)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun logIsCorrect() {
+        var number = 4.0
+        var anotherNumber = 18.0
+
+        if(number.log() == 2 && anotherNumber.log() == 4)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun isNegativeIsCorrect() {
+        var number = 2.0
+
+        if(number.isNegative() == false)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun isPositiveIsCorrect() {
+        var number = 2.0
+
+        if(number.isPositive() == true)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun isInverseCorrect() {
+        var number = 2.0
+
+        if(number.inverse() == 0.5)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun isRoundCorrect() {
+        var number = 2.5
+        var anotherNumber = 2.1
+
+        if(number.round() == 3.0 && anotherNumber.round() == 2.0)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+}
--- app/src/test/java/com/pramonow/numbermathextension/FloatUnitTest.kt
+++ app/src/test/java/com/pramonow/numbermathextension/FloatUnitTest.kt
@@ -1,2 +1,104 @@
 package com.pramonow.numbermathextension
 
+import junit.framework.Assert.assertTrue
+import org.junit.Assert
+import org.junit.Test
+
+class FloatUnitTest {
+
+    @Test
+    fun squareIsCorrect() {
+        var number = 4F
+        var anotherNumber = 10F
+
+        if(number.square() == 16F && anotherNumber.square() == 100F)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun cubicIsCorrect() {
+        var number = 4F
+        var anotherNumber = 10F
+
+        if(number.cubic() == 64F && anotherNumber.cubic() == 1000F)
+            assertTrue(true)
+        else
+            assertTrue(false)
+    }
+
+    @Test
+    fun powerIsCorrect() {
+        var number = 4F
+
+        if(number.power(0) == 1F && number.power(3) == 64F)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun absoluteIsCorrect() {
+        var number = 4F
+        var anotherNumber = -4F
+
+        if(number.absolute() == 4F && anotherNumber.absolute() == 4F)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun logIsCorrect() {
+        var number = 4F
+        var anotherNumber = 18F
+
+        if(number.log() == 2 && anotherNumber.log() == 4)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun isNegativeIsCorrect() {
+        var number = 2F
+
+        if(number.isNegative() == false)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun isPositiveIsCorrect() {
+        var number = 2F
+
+        if(number.isPositive() == true)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun isInverseCorrect() {
+        var number = 2F
+
+        if(number.inverse() == 0.5F)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+    @Test
+    fun isRoundCorrect() {
+        var number = 2.5F
+        var anotherNumber = 2.1F
+
+        if(number.round() == 3F && anotherNumber.round() == 2F)
+            Assert.assertTrue(true)
+        else
+            Assert.assertTrue(false)
+    }
+
+}
